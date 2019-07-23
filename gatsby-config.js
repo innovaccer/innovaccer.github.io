e09313f7cfaeec0c112dc1bd38d8241dc7b3d9b4
Satyam Yadav <satyamyadav3@gmail.com>
2019-07-23 20:20:05 +0530

    init

---
+++ gatsby-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  siteMetadata: {
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        includePaths: ["src/styles"],
+      }
+    },
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import '../styles/index.scss';
+
+const IndexPage = () => (
+   <div>
+       Foo
+   </div>
+)
+
+export default IndexPage
